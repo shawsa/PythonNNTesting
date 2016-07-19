5f31fdd24d6c602959193d4341f5e4de7c87872e
Sage Shaw <sage.b.shaw@gmail.com>
2016-07-19 14:59:10 -0700

    Successful Xor NN.

---
+++ BackPropNN.py
@@ -0,0 +1,96 @@
+import numpy as np
+from random import random
+
+def sigmoid(z):
+		return 1.0/(1.0+np.exp(-z))
+def serial_derivative(z):
+	return sigmoid(z)*(1-sigmoid(z))
+	
+transfer = np.vectorize(sigmoid)
+derivative = np.vectorize(serial_derivative)
+
+class BackPropNN:
+	weights = []
+	bias = []
+	def __init__(self,inputs, outputs, hidden_layers, hidden_layer_size, learning_rate):
+		self.inputs = inputs
+		self.hidden_layers = hidden_layers
+		self.hidden_layer_size = hidden_layer_size
+		self.outputs = outputs
+		self.learning_rate = learning_rate
+		weights = []
+		bias = []
+		for l in range(1+hidden_layers):
+			matrix_rows = hidden_layer_size
+			if l==hidden_layers: matrix_rows = outputs
+			matrix_cols = hidden_layer_size
+			if l==0: matrix_cols = inputs
+			matrix = ()
+			bias_vector = ()
+			for r in range(matrix_rows):
+				row = ()
+				for c in range(matrix_cols):
+					row += (2*random()-1,)
+				matrix += (row,)
+				bias_vector += (2*random()-1,)
+			weights.append(np.matrix(matrix))
+			bias.append(np.matrix(bias_vector).T)
+		self.weights = weights
+		self.bias = bias
+		
+	def output(self, arr_inputs):
+		vector = np.matrix(arr_inputs).T
+		for l in range(self.hidden_layers+1):
+			vector = transfer(self.weights[l] * vector + self.bias[l])
+		return vector
+	
+	def print_network(self):
+		for l in range(self.hidden_layers+1):
+			print("Layer ",l)
+			print("weights")
+			print(self.weights[l])
+			print("bias")
+			print(self.bias[l])
+	
+	def back_prop(self, arr_inputs, arr_outputs):
+		activation = []
+		weighted_inputs = []
+		gradient = []
+		
+		#feed forward
+		vector = np.matrix(arr_inputs).T
+		for l in range(self.hidden_layers + 1):
+			vector = self.weights[l] * vector + self.bias[l]
+			weighted_inputs.append(vector)
+			vector = transfer(vector)
+			activation.append(vector)
+			#placeholder for gradient
+			gradient.append(np.matrix(np.zeros(vector.size)).T)
+		#we would like activation[-1] to contain the input. In python appeding
+		#the input to the end of the list acomplishes this.
+		activation.append(np.matrix(arr_inputs).T)
+		
+		#back propagate
+		gradient[self.hidden_layers] = np.multiply(vector - np.matrix(arr_outputs).T, 
+			derivative(weighted_inputs[self.hidden_layers]))
+		'''print("target: ",arr_outputs)
+		print("output: ",vector)
+		print("gradient")
+		print(gradient[self.hidden_layers])'''
+		
+		for l in range(self.hidden_layers-1,-1,-1):
+			gradient[l] = np.multiply(self.weights[l+1].T * gradient[l+1], derivative(weighted_inputs[l]))
+		
+		#update weights
+		for l in range(self.hidden_layers + 1):
+			'''print("Layer ",l)
+			print("activation")
+			print(activation[l])
+			print("weighted inputs")
+			print(weighted_inputs[l])
+			print("gradient")
+			print(gradient[l])'''
+			self.weights[l] = self.weights[l] - self.learning_rate * (gradient[l]*(activation[l-1].T))
+			self.bias[l] = self.bias[l] - self.learning_rate * gradient[l]
+		#print("new: ", self.output(arr_inputs))
+			
--- XorNN.py
+++ XorNN.py
@@ -1,97 +1,20 @@
-import os, sys
-import numpy as np
-from random import random
-from math import exp
-
-inputs = 2
-outputs = 1
-layer_size = 2
-hidden_layers = 1
-learning_rate = 1
-
-
-#access weights
-#file = open(XorNNWeights.txt,'r')
-
-#randomly assign weights
-weights = []
-for l in range(1+hidden_layers):
-	matrix_rows = layer_size
-	if l==hidden_layers: matrix_rows = outputs
-	matrix_cols = layer_size
-	if l==0: matrix_cols = inputs
-	matrix = ()
-	for r in range(matrix_rows):
-		row = ()
-		for c in range(matrix_cols):
-			row += (random(),)
-		matrix += (row,)
-	weights.append(np.matrix(matrix))
-
-def print_weights():
-	for l in range(1+hidden_layers):
-		print("Layer: ",l)
-		print(weights[l])
-	
-def sigmoid(z):
-	return 1/(1+exp(-z))
-def serial_derivative(z):
-	return sigmoid(z)*(1-sigmoid(z))
-	
-transfer = np.vectorize(sigmoid)
-derivative = np.vectorize(serial_derivative)
-	
-def output(arr):
-	vector = np.matrix(arr).T
-	for l in range(1+hidden_layers):
-		vector = weights[l] * vector
-		vector = transfer(vector)
-	return vector
-	
-def back_propagate(arr_in, arr_target):
-	global weights
-	weighted_input = []
-	activation = []
-	gradient = []
-	
-	#feed forward
-	vector = np.matrix(arr_in).T
-	#activation.append(vector)
-	for l in range(1+hidden_layers):
-		vector = weights[l] * vector
-		weighted_input.append(vector)
-		vector = transfer(vector)
-		activation.append(vector)
-		gradient.append(np.matrix(np.zeros(len(weights[l]))).T)
-		
-	#back propagate
-	difference = activation[hidden_layers] - np.matrix(arr_target)
-	gradient[hidden_layers] = np.multiply(difference, derivative(weighted_input[hidden_layers]))
-	for l in range(hidden_layers-1,-1,-1):
-		gradient[l] = np.multiply(weights[l+1].T*gradient[l+1], derivative(weighted_input[l]))
-	
-	#update weights
-	for l in range(1+hidden_layers):
-		'''print("gradient",l)
-		print(gradient[l])'''
-		diff = gradient[l]*(activation[l].T)
-		diff = (learning_rate/len(weights[l])) * diff
-		weights[l] = weights[l] - diff
-	
-print_weights()
-print("(0,0)",output((0,0)))
-print("(1,0)",output((1,0)))
-print("(0,1)",output((0,1)))
-print("(1,1)",output((1,1)))
-for i in range(10000):
-    back_propagate((0,0),(0))
-    back_propagate((1,0),(1))
-    back_propagate((0,1),(1))
-    back_propagate((1,1),(0))
-print_weights()
-print("(0,0)",output((0,0)))
-print("(1,0)",output((1,0)))
-print("(0,1)",output((0,1)))
-print("(1,1)",output((1,1)))
-
-		
+from BackPropNN import *
+nn = BackPropNN(2,1,1,3,.05)
+#nn.print_network()
+print("Outputs")
+print("(0,0)", nn.output((0,0)))
+print("(1,0)", nn.output((1,0)))
+print("(0,1)", nn.output((0,1)))
+print("(1,1)", nn.output((1,1)))
+print("Training...")
+for i in range(30000):
+	nn.back_prop((0,0),(0))
+	nn.back_prop((1,0),(1))
+	nn.back_prop((0,1),(1))
+	nn.back_prop((1,1),(0))
+#nn.print_network()
+print("Outputs")
+print("(0,0)", nn.output((0,0)))
+print("(1,0)", nn.output((1,0)))
+print("(0,1)", nn.output((0,1)))
+print("(1,1)", nn.output((1,1)))
